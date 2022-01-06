1de14b8a5a54f199e3a398f5b8c2333975feebc4
munis-kim <79528837+munis-kim@users.noreply.github.com>
2022-01-06 21:07:30 +0900

    Add files via upload

---
+++ 10000번 이후/15665번.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<int> v;
+int arr[9];
+
+void solve(int n, int m) {
+	if (m == M) {
+		for (int i = 0; i < M; ++i)
+			cout << arr[i] << ' ';
+		cout << '\n';
+	}
+	else {
+		int check = -1;
+		for (int i = 0; i < N; ++i) {
+			if (check != v[i]) {
+				check = v[i];
+				arr[m] = v[i];
+				solve(i, m + 1);
+			}
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);	cout.tie(NULL);
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N; ++i) {
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	sort(v.begin(), v.end());
+
+	solve(0, 0);
+
+	return 0;
+}
