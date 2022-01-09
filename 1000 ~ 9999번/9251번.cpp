bc27418f1c6012d7f40defa7ad5a039f1c2f6eda
munis-kim <79528837+munis-kim@users.noreply.github.com>
2022-01-09 16:06:24 +0900

    Add files via upload

---
+++ 1000 ~ 9999번/9251번.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#define MAX 1001
+using namespace std;
+
+int dp[MAX][MAX];
+
+int main() {
+	string a, b;
+	cin >> a >> b;
+
+	int a_len = a.size();
+	int b_len = b.size();
+
+	for (int i = 1; i <= a_len; ++i) {
+		for (int j = 1; j <= b_len; ++j) {
+			if (a[i - 1] == b[j - 1])	dp[i][j] = dp[i - 1][j - 1] + 1;
+			else
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+		}
+	}
+
+	cout << dp[a_len][b_len];
+
+	return 0;
+}
